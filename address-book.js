881da7e0ad4d6d66e2ee4d68ebccaaf2e8cf49e7
Nora Top <top.nora@gmail.com>
2015-09-18 16:17:58 +0000

    add main menu question

--- address-book.js
+++ address-book.js
@@ -1,1 +1,12 @@
 //Command-line address book program
+
+var inquirer = require("inquirer");
+
+inquirer.prompt([{
+        type: 'list',
+        name: 'mainMenu',
+        message: 'What do you want to do?',
+        choices: ['Create a new address book entry','Search for existing address book entries','Exit the program']
+    }],function(answers){
+        console.log(answers);
+});
