b65ea28cfaa51a074e5fe714c1a3d7e7e36744c4
Nora Top <top.nora@gmail.com>
2015-09-18 16:27:11 +0000

    add exit program action

--- address-book.js
+++ address-book.js
@@ -2,11 +2,23 @@
 
 var inquirer = require("inquirer");
 
+//start the prompt to the user
 inquirer.prompt([{
         type: 'list',
         name: 'mainMenu',
         message: 'What do you want to do?',
         choices: ['Create a new address book entry','Search for existing address book entries','Exit the program']
-    }],function(answers){
-        console.log(answers);
+    }],function(mainMenuAnswer){
+        
+        //if answer to the main menu is 'Create a new address book entry'
+        if (mainMenuAnswer.mainMenu === 'Create a new address book entry') {
+           console.log(mainMenuAnswer);
+        }
+
+        //if answer to the main menu is 'Search for existing address book entries'
+        if (mainMenuAnswer.mainMenu === 'Search for existing address book entries') {
+           console.log(mainMenuAnswer);
+        }
+        
+        //if answer to the main menu is Exit the program, nothing is done
 });
