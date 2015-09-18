f9c99164dc2b886e693375e33c45e3e766029eb1
Nora Top <top.nora@gmail.com>
2015-09-18 16:35:23 +0000

    add return to the main menu unless exit the program is chosen

--- address-book.js
+++ address-book.js
@@ -3,22 +3,27 @@
 var inquirer = require("inquirer");
 
 //start the prompt to the user
-inquirer.prompt([{
+function startProgram(){inquirer.prompt([{
         type: 'list',
         name: 'mainMenu',
-        message: 'What do you want to do?',
+        message: 'Main menu: choose an option',
         choices: ['Create a new address book entry','Search for existing address book entries','Exit the program']
     }],function(mainMenuAnswer){
+        //console.log(mainMenuAnswer);
         
         //if answer to the main menu is 'Create a new address book entry'
         if (mainMenuAnswer.mainMenu === 'Create a new address book entry') {
-           console.log(mainMenuAnswer);
+            //console.log(mainMenuAnswer);
+            startProgram();
         }
 
         //if answer to the main menu is 'Search for existing address book entries'
         if (mainMenuAnswer.mainMenu === 'Search for existing address book entries') {
-           console.log(mainMenuAnswer);
+            console.log(mainMenuAnswer);
+            startProgram();
         }
         
         //if answer to the main menu is Exit the program, nothing is done
-});
+})}
+
+startProgram();
